59a94f49bc77e611fd6c715dfc35ed36937a2314
Cicada <hubbleadd@bk.ru>
2023-05-28 23:44:56 +0700

    Easy: Add 'Palindrome Number solution'

---
+++ easy/Palindrome-Number.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+class Solution {
+  public:
+    bool isPalindrome(int x) {
+        std::string num = std::to_string(x);
+        long unsigned n = 0;
+        auto beg = num.cbegin();
+        auto end = num.crbegin();
+        while (n != num.size() / 2) {
+            if (*beg != *end) return 0;
+            beg++;
+            end++;
+            n++;
+        }
+        return 1;
+    }
+};
