4174daa8b384ed382ad12d6e16f43c6aa67a7c2b
Cicada <hubbleadd@bk.ru>
2023-05-29 10:02:17 +0700

    Medium: Add 'Container With Most Water' solution

---
+++ medium/Container-With-Most-Water.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+class Solution {
+  public:
+    int maxArea(std::vector<int> &height) {
+        int count = height.size() - 1;
+        int amount = 0;
+        int m = 0;
+
+        auto left = height.begin(), right = height.end() - 1;
+
+        while (left < right) {
+            amount = count * ((*left < *right) ? *left : *right);
+
+            if (m < amount) m = amount;
+
+            if (*left < *right) {
+                ++left;
+                --count;
+            } else if (*right < *left) {
+                --right;
+                --count;
+            } else {
+                ++left;
+                --right;
+                count -= 2;
+            }
+        }
+        return m;
+    }
+};
+
+int main() {}
