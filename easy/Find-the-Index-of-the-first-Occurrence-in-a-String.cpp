311e3a3f35d08ae184819cba937aa94f6c197431
Cicada <hubbleadd@bk.ru>
2023-06-05 10:23:17 +0700

    Easy: Add 'Find the Index of the First Occurrence in a String'

---
+++ easy/Find-the-Index-of-the-first-Occurrence-in-a-String.cpp
@@ -0,0 +1,34 @@
+//-----------------------------------------------------------------------------
+//
+// Source code for 'Find the Index of the First Occurrece in a String' task
+// URL: https://leetcode.com/problems/find-the-index-of-the-first-occurence-in-a-string/
+//
+//-----------------------------------------------------------------------------
+
+#include <string>
+
+#include <iostream>
+
+class Solution {
+  public:
+    int strStr(std::string haystack, std::string needle) {
+        if (needle.size() > haystack.size()) return -1;
+        size_t c = 0, buf;
+
+        for (size_t i = 0; i < haystack.size(); ++i) {
+            c = 0;
+            buf = i;
+            while (haystack.at(i) == needle.at(c)) {
+                if (c == needle.size() - 1) return (i - needle.size() + 1);
+                ++c;
+                ++i;
+                if (i > haystack.size() - 1) break;
+            }
+            i = buf;
+        }
+
+        return -1;
+    }
+};
+
+int main() {}
